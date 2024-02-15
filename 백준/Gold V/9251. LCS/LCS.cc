633bd8faba78251a390547e02c74e2bf3e2f9a98
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-02-15 14:24:34 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold V/9251. LCS/LCS.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#define MAX 1001
+using namespace std;
+
+int dp[MAX][MAX];
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	string a, b;
+	cin >> a >> b;
+
+	for (int i = 1; i <= a.length(); ++i)
+	{
+		for (int j = 1; j <= b.length(); ++j)
+		{
+			if (a[i - 1] == b[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+
+	cout << dp[a.length()][b.length()];
+
+	return 0;
+}
