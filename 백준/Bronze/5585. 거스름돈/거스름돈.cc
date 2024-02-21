e3be770b1b2d722de1719839408d142818f1c906
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-02-21 20:31:17 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -6,47 +6,24 @@ int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
+	int a[6] = { 500, 100, 50, 10, 5, 1 };
+
 	int N;
 	cin >> N;
 	
 	int change = 1000 - N;
 	int coin = 0;
 
-	while (change >= 0)
+	for (int i = 0; i < 6; ++i)
 	{
-		if (change >= 500)
-		{
-			change -= 500;
-			coin++;
-		}
-		else if (change < 500 && change >= 100)
-		{
-			change -= 100;
-			coin++;
-		}
-		else if (change < 100 && change >= 50)
-		{
-			change -= 50;
-			coin++;
-		}
-		else if (change < 50 && change >= 10)
-		{
-			change -= 10;
-			coin++;
-		}
-		else if (change < 10 && change >= 5)
-		{
-			change -= 5;
-			coin++;
-		}
-		else
-		{
-			change -= 1;
-			coin++;
-		}
+		if (change < a[i]) continue;
+		if (change <= 0) break;
+
+		coin += change / a[i];
+		change = change % a[i];
 	}
 
-	cout << coin - 1;
+	cout << coin;
 
 	return 0;
 }
