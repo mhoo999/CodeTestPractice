b4de4bbe897afb5d3a324d5cebe9b74cb0f2f6e8
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-03-26 13:55:21 +0900

    [Bronze I] Title: 소인수분해, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> vec;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 2; i <= 10000000; ++i)
+	{
+		if (N % i == 0)
+		{
+			vec.push_back(i);
+			N /= i;
+			i = 1;
+
+			if (N == 1)
+			{
+				break;
+			}
+		}
+	}
+
+	sort(vec.begin(), vec.end());
+
+	for (int i = 0; i < vec.size(); ++i)
+	{
+		cout << vec[i] << '\n';
+	}
+
+	return 0;
+}
