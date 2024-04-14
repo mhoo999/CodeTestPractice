ea2e9c7a2ac95f9f672201b66c117350c2f493d7
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-04-14 13:37:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 52 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int endNum[10001];
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int iter = 1;
+	int num = 666;
+
+	while (iter < 10001)
+	{
+		string toString = to_string(num);
+
+		bool isEndNum = false;
+
+		for (int i = 2; i < toString.size(); ++i)
+		{
+			if (toString[i] == '6' && toString[i - 1] == '6' && toString[i - 2] == '6')
+			{
+				isEndNum = true;
+				break;
+			}
+		}
+
+		if (isEndNum)
+		{
+			endNum[iter] = num;
+			iter++;
+		}
+
+		num++;
+	}
+
+	cout << endNum[N];
+
+	return 0;
+}
