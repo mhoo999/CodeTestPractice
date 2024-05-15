205964958b8bbab9df24bc538d7ddf5133c43afc
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-05-15 16:40:12 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -17,10 +17,10 @@ int main()
 
 	for (int i = 3; i <= n; ++i)
 	{
-		dp[i] = dp[i - 1] + dp[i - 2] % 10007;
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 	}
 
-	cout << dp[n] % 10007;
+	cout << dp[n];
 
 	return 0;
 }
