11bef646ce2ccafaa1ef85796c36b829ace51834
유명훈 (YU MYEONG HOON) <mhoo999@naver.com>
2024-11-19 21:40:08 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,58 +1,59 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
 int N, M;
-int visit[101][101];
-int check[101][101];
-char map[101][101];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+int Maze[100][100];
 
-void BFS(int x, int y)
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
+
+int BFS()
 {
-	visit[x][y] = 1;
 	queue<pair<int, int>> q;
-	q.push(make_pair(x, y));
+	q.push({ 0, 0 });
+	Maze[0][0] = 2;
 
 	while (!q.empty())
 	{
 		int x = q.front().first;
 		int y = q.front().second;
-
 		q.pop();
 
-		for (int i = 0; i < 4; ++i)
+		for (int i = 0; i < 4; i++)
 		{
-			int next_x = dx[i] + x;
-			int next_y = dy[i] + y;
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 
-			if (next_x >= 0 && next_x < N && next_y >= 0 && next_y < M)
+			if (nx >= 0 && nx < N && ny >= 0 && ny < M && Maze[nx][ny] == 1)
 			{
-				if (visit[next_x][next_y] == 0 && map[next_x][next_y] == '1')
-				{
-					check[next_x][next_y] = check[x][y] + 1;
-					visit[next_x][next_y] = 1;
-					q.push(make_pair(next_x, next_y));
-				}
+				Maze[nx][ny] = Maze[x][y] + 1;
+				q.push({ nx, ny });
 			}
 		}
 	}
+
+	return Maze[N - 1][M - 1] - 1;
 }
 
 int main()
 {
 	cin >> N >> M;
 
-	for (int i = 0; i < N; ++i)
+	for (int i = 0; i < N; i++)
 	{
-		cin >> map[i];
+		string x;
+		cin >> x;
+
+		for (int j = 0; j < M; j++)
+		{
+			Maze[i][j] = x[j] - '0';
+		}
 	}
 
-	BFS(0, 0);
-
-	cout << (check[N - 1][M - 1] + 1);
+	cout << BFS() << endl;
 
 	return 0;
 }
