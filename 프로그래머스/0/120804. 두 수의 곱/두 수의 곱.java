af863cc3cb26f2d660b718dc8fa7e0263944baae
유명훈 (YU MYEONG HOON) <mhoo999@naver.com>
2025-01-02 09:07:45 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
-
 class Solution {
     public int solution(int num1, int num2) {
-        return num1 * num2;
-    }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int answer = 0;
         
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        if (num1 >= 0 && num1 <= 100 && num2 >=1 && num2 <= 100) {
+            answer = num1 * num2;
+        }
         
-        Solution solution = new Solution();
-        int result = solution.solution(num1, num2);
-        
-        System.out.println(result);
+        return answer;
     }
 }
